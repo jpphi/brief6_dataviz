9a071a07427873d9bf32d6dde5d49da41869db2f
jpphi <jpphi@yahoo.fr>
2020-12-04 18:08:15 +0100

    Plotly, page 2 avec graph mais ne correspond pas à PCA

--- prj/pages/page2.py
+++ prj/pages/page2.py
@@ -21,6 +21,13 @@ from dash.dependencies import Input, Output
 
 from app import app
 
+import pandas as pd
+import plotly.express as px
+
+tableau_complet = pd.read_csv('../data/timesData.csv')
+df2016 = tableau_complet [tableau_complet.year == 2016].iloc[:50,:]
+
+
 layout = html.Div([
     html.H3('Analyse en composantes principales.'),
     dcc.Dropdown(
@@ -32,28 +39,26 @@ layout = html.Div([
         ]
     ),
     html.Div(id='app-2-display-value'),
+    
+    dcc.Graph(id='graph2'),
+
+    
     dcc.Link('Retour à la page 1.', href='/pages/page1')
 ])
 
-
 @app.callback(
-    Output('app-2-display-value', 'children'),
+    Output('graph2', 'figure'),
     Input('app-2-dropdown', 'value'))
-def display_value(value):
+#    Input('year-slider', 'value'))
+def update_figure(value):
     if value== "Graph 1":
-        return(dcc.Graph(src="coyote.png"))
-        return "Graph1 à afficher"
-    if value== "Graph 2":
-        return "Graph 2 à afficher"
-    if value== "Graph 3":
-        return "Graph 3 à afficher"
-    return 'Pas de graphe sélectionné.'
-#    return 'Valeur sélectionnée "{}"'.format(value)
+    #filtered_df = df[df.year == selected_year]
+        fig2 = px.scatter(df2016, x="world_rank", y="research", color="country", hover_name="country")
+        return fig2
+    elif value== "Graph 2":
+        fig2 = px.scatter(df2016, x="world_rank", y="income", color="country", hover_name="country")
+        return fig2
 
-"""
-@app.callback(
-    Output('app-2-display-value', 'children'),
-    Input('app-2-dropdown', 'value'))
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-"""
+    else :
+        fig2 = px.scatter(df2016, x="world_rank", y="teaching", color="country", hover_name="country")
+        return fig2
